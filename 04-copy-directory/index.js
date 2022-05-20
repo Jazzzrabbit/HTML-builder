595742425491a348471221ed4e17060392583552
jazzzrabbit <jazzzrabbit@gmail.com>
2022-05-20 13:37:28 +0300

    feat: add 4th solved problem

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
 
+const readFilesFromDir = async (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, data) => {
+      if (err) reject(err.message);
+      resolve(data);
+    });
+  });
+};
+
+readFilesFromDir(path.join(__dirname, 'files'))
+  .then(fs.mkdir(path.join(__dirname, 'newFiles'), {recursive: true}, (err) => {
+    if (err) console.error(err);
+    console.log('"newFiles" folder was successfully created.');
+  }))
+  .then((data) => {
+    data.forEach(item => {
+      fs.readFile(path.join(__dirname, 'files', item), (err, data) => {
+        if (err) console.error(err);
+        fs.writeFile(path.join(__dirname, 'newFiles', item), data, (err) => {
+          if (err) console.error(err);
+          console.log(`File "${item}" was successfully copied to newFiles.`);
+        });
+      });
+    });
+  });
