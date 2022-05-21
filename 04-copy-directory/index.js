899b1fc2df601584297ec69841b911f6380982b7
jazzzrabbit <jazzzrabbit@gmail.com>
2022-05-21 21:53:53 +0300

    refactor: fix some issues

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,19 +10,52 @@ const readFilesFromDir = async (path) => {
   });
 };
 
-readFilesFromDir(path.join(__dirname, 'files'))
-  .then(fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
-    if (err) console.error(err);
-    console.log('"files-copy" folder was successfully created.');
-  }))
-  .then((data) => {
-    data.forEach(item => {
-      fs.readFile(path.join(__dirname, 'files', item), (err, data) => {
-        if (err) console.error(err);
-        fs.writeFile(path.join(__dirname, 'files-copy', item), data, (err) => {
+const createFolder = async (path) => {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(path, {recursive: true}, (err) => {
+      if (err) reject(err.message);
+      resolve();
+    });
+  });
+};
+
+const cleanFolder = async () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+      if (err) reject(err.message);
+      files.forEach(exFile => {
+        fs.rm(path.join(__dirname, 'files-copy', exFile), (err) => {
           if (err) console.error(err);
-          console.log(`File "${item}" was successfully copied to files-copy.`);
         });
+        resolve();
       });
     });
   });
+};
+
+readFilesFromDir(path.join(__dirname, 'files'))
+  .then(createFolder(path.join(__dirname, 'files-copy')))
+  .then(cleanFolder())
+  .then((data) => {
+    data.forEach(file => {
+      fs.stat(path.join(__dirname, 'files', file), (err, stats) => {
+        if (err) console.error(err);
+        if (stats.isFile()) {
+          fs.readFile(path.join(__dirname, 'files', file), 'utf8', (err, data) => {
+            if (err) console.error(err);
+            fs.writeFile(path.join(__dirname, 'files-copy', file), data, (err) => {
+              if (err) console.error(err);
+              console.log(`${file} was copied to files-copy folder.`);
+            });
+          });   
+        }
+      });
+    });
+  });
+
+
+  
+
+  
+
+     
